62004b66918a1c5f4b321159b4626348faa049df
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2009-09-26 01:01:44 +0530

    Add validation to the survey participation page

---
+++ media/script/survey_participate.js
@@ -0,0 +1,10 @@
+jQuery(function(){
+    jQuery('form.decorated-fields').validate({
+        rules: {
+            mobile_number: {
+                mobile: true
+            }
+        }
+    });
+});
+
