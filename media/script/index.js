e053d49b7d96288e8e8b326e21eb9d1776feb2ad
Doli Bhanushali <doli@happychickoo.com>
2009-09-25 10:00:18 +0530

    Add flashembed scripts to the presentation script on the home page to avoid the related video feed.
    Signed-off-by: Doli Bhanushali <doli@happychickoo.com>

---
+++ media/script/index.js
@@ -0,0 +1,10 @@
+jQuery(function(){
+  flashembed('video_wrapper', {
+      src: 'http://www.youtube.com/v/Xf8VePS4X9I&hl=en&fs=1&ap=%2526fmt%3D18&autoplay=1&rel=0&border=0&loop=0',
+      wmode: 'transparent',
+      loop: 'false',
+      allowFullScreen: 'true',
+      allowscriptaccess: 'always',
+      height: '344px'
+  });
+});
