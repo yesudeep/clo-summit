c909eade3399725db9c9691550891db11b806784
Doli Bhanushali <doli@happychickoo.com>
2009-09-26 14:11:09 +0530

    Integrate twitter feed and style them.
    Signed-off-by: Doli Bhanushali <doli@happychickoo.com>

--- media/script/index.js
+++ media/script/index.js
@@ -1,10 +1,33 @@
-jQuery(function(){
-  flashembed('video_wrapper', {
-      src: 'http://www.youtube.com/v/Xf8VePS4X9I&hl=en&fs=1&ap=%2526fmt%3D18&autoplay=1&rel=0&border=0&loop=0',
-      wmode: 'transparent',
-      loop: 'false',
-      allowFullScreen: 'true',
-      allowscriptaccess: 'always',
-      height: '280px'
-  });
-});
+(function($){
+    jQuery(function(){
+      flashembed('video_wrapper', {
+          src: 'http://www.youtube.com/v/Xf8VePS4X9I&hl=en&fs=1&ap=%2526fmt%3D18&autoplay=1&rel=0&border=0&loop=0',
+          wmode: 'transparent',
+          loop: 'false',
+          allowFullScreen: 'true',
+          allowscriptaccess: 'always',
+          height: '280px'
+      });
+      jQuery("#tweets").tweet({
+        username: ['closummitindia'],
+        count: 2,
+        auto_join_text_default: "we said,",
+        auto_join_text_ed: "we",
+        auto_join_text_ing: "we were",
+        auto_join_text_reply: "we replied to",
+        auto_join_text_url: "we were checking out"
+    });
+      jQuery('#qoutes').innerfade({
+          animationtype: 'fade',
+          speed: 750,
+          timeout: 2000,
+          type: 'random',
+          containerheight: '100px'
+      });
+      jQuery('.toggle_view a[href="advisory_board"]').toggle(function(){
+            jQuery('#advisory_board').css('display', 'none');
+        }, function(){
+            jQuery('#advisory_board').css('display', 'block');
+        });
+    });
+})(jQuery);
