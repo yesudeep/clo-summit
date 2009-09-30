be0eaac3877ae9726f2be1897d999476914afd79
Doli Bhanushali <doli@happychickoo.com>
2009-09-30 19:03:09 +0530

    Make tabs work on the program and about page.
    Signed-off-by: Doli Bhanushali <doli@happychickoo.com>

--- media/script/program.js
+++ media/script/program.js
@@ -1,4 +1,7 @@
 jQuery(function(){
-    jQuery("#accordion").tabs("#accordion .pane", {event: 'mouseover', tabs: 'h2.tab', effect: 'slide'});
+    //jQuery("#accordion").tabs("#accordion .pane", {event: 'mouseover', tabs: 'h2.tab', effect: 'slide'});
+    jQuery("#tabs").tabs("#panes > li", {
+          event: 'mouseover'
+    });
 });
 
