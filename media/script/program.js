16dc2b78e36cbb4846e217ab7f1866e48731414f
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2009-09-30 11:05:32 +0530

    display accordion on mouseover

--- media/script/program.js
+++ media/script/program.js
@@ -1,4 +1,4 @@
 jQuery(function(){
-    jQuery("#accordion").tabs("#accordion .pane", {tabs: 'h2.tab', effect: 'slide'});
+    jQuery("#accordion").tabs("#accordion .pane", {event: 'mouseover', tabs: 'h2.tab', effect: 'slide'});
 });
 
