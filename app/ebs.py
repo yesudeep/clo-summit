bd80a4b379c5a9440e7961d7ff7b49ef496959e0
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2009-10-09 11:33:42 +0530

    ignore ebs_data.py because it contains sensitive information.

--- app/ebs.py
+++ app/ebs.py
@@ -26,7 +26,7 @@
 from hc_gae_util.collections.propertydict import PropertyDict
 from urllib import urlencode
 
-MODE_PRODUCTION = 'LIVE'
+MODE_PRODUCTION = 'TEST'
 MODE_DEVELOPMENT = 'TEST'
 
 PAYMENT_GATEWAY_URL = 'https://secure.ebs.in/pg/ma/sale/pay/'
