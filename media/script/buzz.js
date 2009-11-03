5c4e33e8ec755a9d09cf0df43a6f82cdd35051dc
Doli Bhanushali <doli@happychickoo.com>
2009-11-03 13:15:49 +0530

    Add images of people and media partners on the buzz(media) page.
    Signed-off-by: Doli Bhanushali <doli@happychickoo.com>

---
+++ media/script/buzz.js
@@ -0,0 +1,11 @@
+jQuery(function(){
+    jQuery("#media_people").innerfade({
+      animationtype: 'fade',
+      speed: 'slow',
+      timeout: 4000,
+      type: 'sequence',
+      containerheight: '400px'
+    });
+});
+
+    
