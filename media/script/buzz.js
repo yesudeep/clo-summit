7b57070fce1fdb8862e76831099c4a95a324d429
Doli Bhanushali <doli@happychickoo.com>
2009-11-03 13:51:37 +0530

    Increase the delay of innerfade to 5 sec in .js file.
    Signed-off-by: Doli Bhanushali <doli@happychickoo.com>

--- media/script/buzz.js
+++ media/script/buzz.js
@@ -2,7 +2,7 @@ jQuery(function(){
     jQuery("#media_people").innerfade({
       animationtype: 'fade',
       speed: 'slow',
-      timeout: 4000,
+      timeout: 5000,
       type: 'sequence',
       containerheight: '250px'
     });
