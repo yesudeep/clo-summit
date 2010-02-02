e6282b1e6dcb62f794675966e4d7137d2771b7f1
Doli Bhanushali <doli@waterloo.(none)>
2010-02-02 20:32:24 +0530

    Get the video playlist working.
    Signed-off-by: Doli Bhanushali <doli@waterloo.(none)>

--- media/script/index.js
+++ media/script/index.js
@@ -1,34 +1,35 @@
-jQuery(function(){
-  flashembed('video_wrapper', {
-      src: 'http://www.youtube.com/v/pTRwPs_Aqr0&hl=en&fs=1&ap=%2526fmt%3D18&autoplay=1&rel=0&border=0&loop=0',
-      wmode: 'transparent',
-      loop: 'false',
-      allowFullScreen: 'true',
-      allowscriptaccess: 'always',
-      height: '280px'
-  });
-  jQuery("#tweets").tweet({
-    username: ['closummitindia'],
-    count: 2,
-    auto_join_text_default: "we said,",
-    auto_join_text_ed: "we",
-    auto_join_text_ing: "we were",
-    auto_join_text_reply: "we replied to",
-    auto_join_text_url: "we were checking out"
-  });
-  jQuery('#qoutes').innerfade({
-      animationtype: 'fade',
-      speed: 'slow',
-      timeout: 6000,
-      type: 'sequence',
-      containerheight: '120px'
-  });
-  jQuery('#theme').innerfade({
-      animationtype: 'slide',
-      speed: 'slow',
-      timeout: 2000,
-      type: 'sequence',
-      containerheight: '88px'
-  });
+ jQuery(function(){
+ flashembed('video_wrapper', {
+     src: 'http://www.youtube.com/cp/vjVQa1PpcFP0swE3qxYKvsq39pbnf9qow-F2s-62UUo=&autoplay=1&rel=0&border=0&loop=0',
+     wmode: 'transparent',
+     loop: 'false',
+     allowFullScreen: 'true',
+     allowscriptaccess: 'always',
+     height: '280px'
+ });
+ jQuery("#tweets").tweet({
+   username: ['closummitindia'],
+   count: 2,
+   auto_join_text_default: "we said,",
+   auto_join_text_ed: "we",
+   auto_join_text_ing: "we were",
+   auto_join_text_reply: "we replied to",
+   auto_join_text_url: "we were checking out"
+ });
+ jQuery('#qoutes').innerfade({
+     animationtype: 'fade',
+     speed: 'slow',
+     timeout: 6000,
+     type: 'sequence',
+     containerheight: '120px'
+ });
+ jQuery('#theme').innerfade({
+     animationtype: 'slide',
+     speed: 'slow',
+     timeout: 2000,
+     type: 'sequence',
+     containerheight: '88px'
+ });
 });
+ 
 
